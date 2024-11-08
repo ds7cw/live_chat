b1d7058b3b026d6f1956b13e2fdfb806bea64cd8
ds7cw <delyan.shivachev@hotmail.com>
2024-11-08 22:03:16 +0000

    Create API endpoint decorator for detailing endpoints

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,33 +1,34 @@
 from django.db.models import Count
 from rest_framework import viewsets
-from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Server
+from .schema import server_list_docs
 from .serializer import ServerSerializer
 
 
 # Create your views here.
 class ServerListViewSet(viewsets.ViewSet):
-
     queryset = Server.objects.all()
 
+    @server_list_docs
     def list(self, request):
         category = request.query_params.get('category')
         qty = request.query_params.get('qty')
         by_user = request.query_params.get('by_user') == 'true'
-        by_server_id = request.query_params.get('by_server_id') == 'true'
+        by_server_id = request.query_params.get('by_server_id')
         with_num_members = request.query_params.get('with_num_members') == 'true'
-
-        if by_user and not request.user.is_authenticated:
-            raise AuthenticationFailed(detail='You need to be signed in')
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
         if by_user:
-            user_id = request.user.id
-            self.queryset = self.queryset.filter(member=user_id)
+            if request.user.is_authenticated:
+                user_id = request.user.id
+                self.queryset = self.queryset.filter(member=user_id)
+            else:
+                raise AuthenticationFailed(detail='You need to be signed in')
 
         if with_num_members:
             self.queryset = self.queryset.annotate(num_members=Count('member'))
@@ -37,10 +38,9 @@ class ServerListViewSet(viewsets.ViewSet):
 
         if by_server_id:
             try:
-                server_id = int(request.query_params.get('server_id'))
-                self.queryset = self.queryset.filter(id=server_id)
-                if not self.queryset:
-                    raise ValidationError(detail=f'Server with id {server_id} not found')
+                self.queryset = self.queryset.filter(id=int(by_server_id))
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f'Server with id {by_server_id} not found')
             except ValueError:
                 raise ValidationError(detail='Incorrect server_id input type')
 
