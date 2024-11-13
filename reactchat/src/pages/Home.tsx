a1d4f53ddc7c57bbbc0855d9ab38d63d27b5c491
ds7cw <delyan.shivachev@hotmail.com>
2024-11-13 07:26:25 +0000

    Create primary app toolbar & integrate google fonts

--- reactchat/src/pages/Home.tsx
+++ reactchat/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { Box, CssBaseline } from "@mui/material";
+import PrimaryAppBar from "./templates/PrimaryAppBar";
 
 const Home = () => {
     return (
         <Box sx={{ display: "flex" }}>
             <CssBaseline />
-            Home
+            <PrimaryAppBar />
         </Box>
     );
 };
