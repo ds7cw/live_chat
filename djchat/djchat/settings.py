66f49010e52ebbe6e2fa69638b50240508469605
ds7cw <delyan.shivachev@hotmail.com>
2024-11-21 07:28:59 +0000

    Install cors-headers & configure allowed origins

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -42,6 +42,7 @@ USER_APPS = [
 EXTERNAL_APPS = [
     'drf_spectacular',
     'rest_framework',
+    'corsheaders',
 ]
 
 INSTALLED_APPS = [
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # Place above CommonMw
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -153,3 +155,7 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': True,
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
