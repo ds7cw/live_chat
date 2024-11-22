3c70decfb93c3cc379d229798ec1d029e02a363c
ds7cw <delyan.shivachev@hotmail.com>
2024-11-22 20:27:31 +0000

    Add permission so API is accessible to registered users only

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -2,6 +2,7 @@ from django.db.models import Count
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Server
 from .schema import server_list_docs
@@ -11,6 +12,7 @@ from .serializer import ServerSerializer
 # Create your views here.
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
+    permission_classes = [IsAuthenticated]
 
     @server_list_docs
     def list(self, request):
