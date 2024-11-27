82d4cc65dc7f459eebb417d79449d0df12b79c2c
ds7cw <delyan.shivachev@hotmail.com>
2024-11-27 07:56:29 +0000

    Add ExploreServer component, displays server details

--- reactchat/src/App.tsx
+++ reactchat/src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "@emotion/react";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import Explore from "./pages/Explore";
 import {createMuiTheme } from "./theme/theme";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Home />}/>
+      <Route path="/explore/:categoryName" element={<Explore />}/>
     </Route>
   )
 );
