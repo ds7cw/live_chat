38d288322e2e7121d8107272f76b9e0f43862dfa
ds7cw <delyan.shivachev@hotmail.com>
2024-11-28 19:01:20 +0000

    Add dark mode theme & switch button

--- reactchat/src/App.tsx
+++ reactchat/src/App.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import {createMuiTheme } from "./theme/theme";
+import ToggleColorMode from "./components/ToggleColorMode";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,11 +15,10 @@ const router = createBrowserRouter(
 );
 
 const App = () => {
-  const theme = createMuiTheme();
   return (
-    <ThemeProvider theme={theme}>
+    <ToggleColorMode>
       <RouterProvider router={router} />
-    </ThemeProvider>
+    </ToggleColorMode>
   );
 };
 
