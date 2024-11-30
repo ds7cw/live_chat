608c2339fe02ba1d493cff7a55326ed0f44c7b55
ds7cw <delyan.shivachev@hotmail.com>
2024-11-30 09:46:08 +0000

    Set up Django websocket consumer

--- djchat/djchat/asgi.py
+++ djchat/djchat/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djchat.settings')
+django_application = get_asgi_application()
 
-application = get_asgi_application()
+from . import urls # placement after .settings is intentional
+
+
+# application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        'http': get_asgi_application(),
+        'websocket': URLRouter(urls.websocket_urlpatterns),
+    }
+)
